6eb7a7f4ff641622e8fef49e99df2997c8e8b165
Erkan Ozcan <48063211+Erkanx@users.noreply.github.com>
2022-07-05 13:50:25 -0500

    Added notes.js file
    Refactored code and added new file into the folder

--- Node-Practice/app.js
+++ Node-Practice/app.js
@@ -1,4 +1,2 @@
-const fs = require('fs');
-
-// fs.writeFileSync('notes.txt', 'This file was created by Erkan.');
-fs.appendFileSync('notes.txt', ' I live food.');
+const notes = require('./notes');
+console.log(notes.getNotes());
