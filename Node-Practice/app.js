fb3dcdc778bed7e5afaaab76a69da7a48b437796
Erkan Ozcan <48063211+Erkanx@users.noreply.github.com>
2022-07-05 13:00:30 -0500

    Added appendFileSync
    Appended a message to the file notes.txt

--- Node-Practice/app.js
+++ Node-Practice/app.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'This file was created by Erkan')
+// fs.writeFileSync('notes.txt', 'This file was created by Erkan.');
+fs.appendFileSync('notes.txt', ' I live food.');
