b2aa40fb2466d387ddc65fe7f6426c38730ee9cb
Erkan Ozcan <48063211+Erkanx@users.noreply.github.com>
2022-07-06 10:03:24 -0500

    Added Chalk from NPM
    Chalk allows us to change colors of our messages.

--- Node-Practice/app.js
+++ Node-Practice/app.js
@@ -1,7 +1,12 @@
 const notes = require('./notes');
-const validator = require('validator');
+//const validator = require('validator');
+const chalk = require('chalk');
 
 
 console.log(notes.getNotes());
 
-console.log(validator.isURL('https://google.com'));
+//console.log(validator.isURL('https://google.com'));
+
+console.log(chalk.green.inverse('Success!'));
+
+console.log(chalk.green('Success!'));
