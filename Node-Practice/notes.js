0f19033af3c5f6357976484f73ed1d7664a0ba16
Erkan Ozcan <48063211+Erkanx@users.noreply.github.com>
2022-07-07 09:32:37 -0500

    Refactor code and working with JSON
    Created two new functions that can load new information to JSON file and added validations.

--- Node-Practice/notes.js
+++ Node-Practice/notes.js
@@ -1,3 +1,38 @@
+const fs = require('fs');
+
 module.exports.getNotes = () => {
     return 'Your Notes...'
+}
+
+module.exports.addNote = (title, body) => {
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter((note) => {
+        return note.title === title;
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes);
+        console.log('New Note Added');
+    } else {
+        console.log('Note title taken!');
+    }
+}
+
+saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (error) {
+        return [];
+    }
 }
